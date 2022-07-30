98f7c3c876d05eb7c7cc14e34c75eaa5c1be0063
unknown <mindc1may85@iiht.tech>
2022-07-30 01:43:20 -0700

    first commit

---
+++ src/test/java/com/Employee/Employeegroupp/EmployeeGrouppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Employee.Employeegroupp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeGrouppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
